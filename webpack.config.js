89f302fba6b306bd60adaf70e2b9e11dec4adea7
Marcelo Oggero <marcelooggero@gmail.com>
2021-01-28 22:39:37 -0300

    added pwa

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const DotenvWebpackPlugin = require("dotenv-webpack");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -53,6 +54,13 @@ module.exports = {
     }),
     new DotenvWebpackPlugin({
       path: './config.env'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '' },
+        { from: 'public/service-worker.js', to: '' },
+        { from: 'public/icon.png', to: 'assets' },
+      ],
     }),
   ],
   devServer: {
